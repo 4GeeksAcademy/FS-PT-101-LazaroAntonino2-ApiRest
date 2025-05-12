dee744a359963c6dff3222994bc2b4ad6d322a6b
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-05-12 12:58:09 +0000

    fc

--- src/app.py
+++ src/app.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from sqlalchemy import select
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Profile, Car
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +37,183 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# GET ALL USERS ----->
+@app.route('/users', methods=['GET'])   
+def get_users():
+    stmt = select(User)
+    users = db.session.execute(stmt).scalars().all()
+    return jsonify([user.serialize() for user in users]), 200
 
-    return jsonify(response_body), 200
+
+# GET SINGLE USER ----->
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    return jsonify(user.serialize()),200
+
+
+# POST USER ------->
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if not data or 'email' not in data or 'password' not in data or 'age' not in data:
+        return jsonify({'error':'Missing data'}), 400
+
+    # Crear usuario
+    new_user = User(
+        email=data['email'],
+        password=data['password'],
+        age=data['age']
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize()), 200
+
+# DELETE USER 
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    if user.profile:
+        db.session.delete(user.profile)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message':'user deleted'}),200
+
+# PUT USER
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    user.email = data.get('email',user.email)
+    user.age = data.get('age',user.age)
+    user.password = data.get('password',user.password)
+    db.session.commit()
+    return jsonify(user.serialize()),200
+
+
+# GET ALL PROFILES
+@app.route('/users/profile', methods=['GET'])   
+def get_users_profile():
+    stmt = select(Profile)
+    users = db.session.execute(stmt).scalars().all()
+    return jsonify([user.serialize() for user in users]), 200
+
+
+
+# GET SINGLE USER PROFILE ----->
+@app.route('/users/<int:user_id>/profile', methods=['GET'])
+def get_single_user_profile(user_id):
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    return jsonify(user.profile.serialize()),200
+
+
+# PUT SINGLE USER PROFILE ------>
+@app.route('/users/<int:user_id>/profile', methods=['PUT'])
+def update_user_profile(user_id):
+    data = request.get_json()
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    
+    if user is None:
+        return jsonify({'error':'User not found'}), 404
+    if user.profile is None:
+        return jsonify({'error':'This user has not profile create it using a "POST"'})
+    
+    user.profile.title = data.get('title',user.profile.title)
+    user.profile.bio = data.get('bio',user.profile.bio)
+    db.session.commit()
+    return jsonify(user.profile.serialize()),200
+
+
+# DELETE USER PROFILE ------>
+@app.route('/users/<int:user_id>/profile', methods=['DELETE'])
+def delete_user_profile(user_id):
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+
+    if user.profile is None:
+        return jsonify({'error': 'This user do not have a profile'}), 404
+
+    db.session.delete(user.profile)
+    db.session.commit()
+
+    return jsonify({'message': 'User profile deleted'}), 200
+
+# POST SINGLE USE PROFILE ------> 
+@app.route('/users/<int:user_id>/profile', methods=['POST'])
+def create_user_profile(user_id):
+    data = request.get_json()
+
+    if not data or 'title' not in data or 'bio' not in data:
+        return jsonify({'error':'Missing data'}), 400
+    
+    stmt = select(User).where(User.id == user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+    if user.profile:
+        return jsonify({'error': 'User already have a profile, select "PUT" insted of "POST" '}), 404
+    else:
+        new_profile = Profile(title=data['title'], bio=data['bio'])
+        user.profile = new_profile
+    db.session.commit()
+    return jsonify(user.profile.serialize()), 200
+
+
+@app.route('/cars', methods=['GET'])   
+def get_cars():
+    stmt = select(Car)
+    users = db.session.execute(stmt).scalars().all()
+    return jsonify([user.serialize() for user in users]), 200
+
+
+# POST CAR
+@app.route('/users/<int:user_id>/cars', methods=['POST'])
+def create_car(user_id):
+    data = request.get_json()
+    
+    required_fields = ['model', 'year', 'name']
+    if not data or any(field not in data for field in required_fields):
+        return jsonify({'error': 'Missing data'}), 400
+
+    # Comprobar que el usuario exista
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Crear coche asociado al usuario
+    new_car = Car(
+        name=data['name'],
+        year=data['year'],
+        model=data['model'],
+        user_id=user_id
+    )
+
+    db.session.add(new_car)
+    db.session.commit()
+
+    return jsonify(new_car.serialize()), 201
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -1,19 +1,83 @@
+from __future__ import annotations  # permite referencias a clases futuras en tipos
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List, Optional
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
 
+    # Relaciones
+    profile: Mapped[Optional[Profile]] = relationship('Profile', back_populates='user', uselist=False)
+    cars: Mapped[List[Car]] = relationship('Car', back_populates='user')
+    favourites: Mapped[List[Favourite]] = relationship('Favourite', back_populates='user')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "age": self.age,
+            "profile": self.profile.serialize() if self.profile else None,
+            "favourites": [fav.serialize() for fav in self.favourites]
+            # No serializar la contraseña
         }
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(20))
+    bio: Mapped[str] = mapped_column(String(120))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), unique=True)
+
+    user: Mapped[User] = relationship('User', back_populates='profile')
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "title": self.title,
+            "bio": self.bio
+        }
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    model: Mapped[str] = mapped_column(String(20), nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped[User] = relationship('User', back_populates='cars')
+    favourites: Mapped[List[Favourite]] = relationship('Favourite', back_populates='car')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'model': self.model,
+            'year': self.year,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
+class Favourite(db.Model):
+    __tablename__ = 'favourites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    car_id: Mapped[int] = mapped_column(ForeignKey('cars.id'))
+
+    user: Mapped[User] = relationship('User', back_populates='favourites')
+    car: Mapped[Car] = relationship('Car', back_populates='favourites')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'car_id': self.car_id
+        }
+
+
